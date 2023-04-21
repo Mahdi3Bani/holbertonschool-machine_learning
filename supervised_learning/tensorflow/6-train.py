b0214a7cfeae0b8d71bd1eee93b6ecf10698cbac
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-04-21 02:43:13 +0200

    training...

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-builds, trains, and saves a neural network classifier
-"""
+
 
 
 import tensorflow as tf
@@ -12,32 +10,32 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
-          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    """
-builds, trains, and saves a neural network classifier
-    """
-    x, y = create_placeholders(X_train.shape, Y_train.shape[1])
-
+def train(X_train,
+          Y_train,
+          X_valid,
+          Y_valid,
+          layer_sizes,
+          activations,
+          alpha,
+          iterations,
+          save_path="/tmp/model.ckpt"):
+  
+    x, y = create_placeholders(X_train.shape[1],
+                               Y_train.shape[1])
+    tf.add_to_collection("x", x)
+    tf.add_to_collection("y", y)
     y_pred = forward_prop(x, layer_sizes, activations)
-
+    tf.add_to_collection("y_pred", y_pred)
     loss = calculate_loss(y, y_pred)
+    tf.add_to_collection("loss", loss)
     accuracy = calculate_accuracy(y, y_pred)
-
+    tf.add_to_collection("accuracy", accuracy)
     train_op = create_train_op(loss, alpha)
-
-    tf.add_to_collection('x', x)
-    tf.add_to_collection('y', y)
-    tf.add_to_collection('y_pred', y_pred)
-    tf.add_to_collection('loss', loss)
-    tf.add_to_collection('accuracy', accuracy)
-    tf.add_to_collection('train_op', train_op)
-
-
+    tf.add_to_collection("train_op", train_op)
+    init = tf.global_variables_initializer()
     saver = tf.train.Saver()
-
     with tf.Session() as session:
-        session.run(tf.global_variables_initializer())
+        session.run(init)
         for iteration in range(iterations + 1):
             loss_train = session.run(loss,
                                      feed_dict={x: X_train,
@@ -61,4 +59,4 @@ builds, trains, and saves a neural network classifier
                 session.run(train_op, feed_dict={x: X_train,
                                                  y: Y_train})
         save_path = saver.save(session, save_path)
-    return 
+    return save_path
