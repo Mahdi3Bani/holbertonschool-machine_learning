144fcae2b565af10ae33ae5b20445f85c1e476d9
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-04-26 14:00:10 +0200

    Normalization Constants

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""calculates the normalization (standardization) constants of a matrix"""
+
+
+import numpy as np
+
+
+def normalization_constants(X):
+    """calculates the normalization (standardization) constants of a matrix"""
+
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+    return mean, std
