639995e12268c212995391a8a9b1e4480dd92182
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-04-26 16:12:01 +0200

    shuffle

---
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""shuffles the data points in two matrices the same way"""
+
+
+import numpy as np
+"""shuffles the data points in two matrices the same way"""
+
+
+def shuffle_data(X, Y):
+
+    permutation = np.random.permutation(X.shape[0])
+    X_shuffled = X[permutation]
+    Y_shuffled = Y[permutation]
+    return X_shuffled, Y_shuffled
