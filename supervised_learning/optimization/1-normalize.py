29a8d5b0c85ca808e718bc05d90a031ded48e18c
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-04-26 14:15:24 +0200

    normalize

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""normalizes (standardizes) a matrix"""
+
+
+import numpy as np
+
+
+def normalize(X, m, s):
+    """normalizes (standardizes) a matrix"""
+
+    return (X - m) / s
