b8d9ca7a436fb47965dcc9a9419f1371c40be316
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-04-28 14:15:59 +0200

    moving average

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -4,9 +4,9 @@
 
 def moving_average(data, beta):
     """calculates the weighted moving average of a data set"""
-    l = []
+    moving_average_list = []
     f = 0
     for i in range(len(data)):
         f = beta * f + (1 - beta) * data[i]
-        l.append(f / (1 - beta ** (i + 1)))
-    return l
+        moving_average_list.append(f / (1 - beta ** (i + 1)))
+    return moving_average_list
