a553ee7a0fb8bfe1b152ac1c7c43cbe6575e3ef3
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-04-28 13:26:27 +0200

    mini-batch

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -37,7 +37,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5
             for j in range(0, length_of_dataset, batch_size):
                 steps.append((j, j + batch_size))
 
-            for k in range(len(steps)):
+            for k in range(1, len(steps)):
                 start, end = steps[k]
                 x_batch = X_shuff[start:end]
                 y_batch = Y_shuff[start:end]
