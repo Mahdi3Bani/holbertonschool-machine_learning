6546d3948aca8ed8dcbbfe3052f0ce9c49e41a5d
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-05 02:04:15 +0200

    F1 score

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -2,7 +2,6 @@
 """calculates the F1 score for each class in a confusion matrix"""
 
 
-import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('1-precision').precision
 
