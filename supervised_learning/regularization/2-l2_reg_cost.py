b90902afd91f516f2505650da8675974b40b8510
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-08 00:05:04 +0200

    l2

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -11,11 +11,5 @@ def l2_reg_cost(cost):
     Calculates the cost of a neural network with L2 regularization.
     """
 
-    lambtha = 0.01
-    reg_losses = []
-    for var in tf.trainable_variables():
-        if 'kernel' in var.name:
-            reg_losses.append(tf.nn.l2_loss(var))
-
-    l2_loss = lambtha * tf.reduce_sum(reg_losses)
-    return cost + l2_loss
+    
+    return cost + tf.losses.get_regularization_losses()
