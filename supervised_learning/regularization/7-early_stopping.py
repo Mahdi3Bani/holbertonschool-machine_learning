de0d58001702983a1ec902767ed6e0d0b987fc89
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-07 16:51:14 +0200

    Early Stopping

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """determines if you should stop gradient descent early"""
 
+
 def early_stopping(cost, opt_cost, threshold, patience, count):
     """
     determines if you should stop gradient descent early
