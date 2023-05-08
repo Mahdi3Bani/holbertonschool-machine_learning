a7d35d1f5d37301a1cc779f4d25ae13aadf06410
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-08 17:04:04 +0200

    Sequential

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -2,21 +2,22 @@
 """builds a neural network with the Keras library"""
 
 
-from tensorflow import keras
+import tensorflow.keras as K
+#from tensorflow import keras
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """builds a neural network with the Keras library"""
-    model = keras.models.Sequential()
+    model = K.models.Sequential()
 
-    model.add(keras.layers.Dense(layers[0],
+    model.add(K.layers.Dense(layers[0],
                                  activation=activations[0], input_shape=(nx,),
-                                 kernel_regularizer=keras.regularizers.l2(lambtha)))
+                                 kernel_regularizer=K.regularizers.l2(lambtha)))
 
     for i in range(1, len(layers)):
-        model.add(keras.layers.Dense(layers[i],
+        model.add(K.layers.Dense(layers[i],
                                      activation=activations[i],
-                                     kernel_regularizer=keras.regularizers.l2(lambtha)))
+                                     kernel_regularizer=K.regularizers.l2(lambtha)))
         if keep_prob < 1:
-            model.add(keras.layers.Dropout(1-keep_prob))
+            model.add(K.layers.Dropout(1-keep_prob))
     return model
