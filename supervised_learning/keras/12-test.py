614020ac2317ac2fc8bcd37cf713bab827c7b0a2
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-12 00:17:02 +0200

    save and load

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""tests a neural network"""
+
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """tests a neural network"""
+    loss, accuracy = network.evaluate(data, labels, verbose=verbose)
+    return loss, accuracy
