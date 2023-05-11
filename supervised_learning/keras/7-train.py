474bd6532c709c3636710a777b83f193f004dfaa
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-11 16:59:13 +0200

    Train

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -7,21 +7,18 @@ import tensorflow.keras as K
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
-                patience=0,learning_rate_decay=False,
+                patience=0, learning_rate_decay=False,
                 alpha=0.1, decay_rate=1, verbose=True,
                 shuffle=False):
     """update the previous function and
     to train the model with learning rate decay"""
-    callbacks = []
+    callback = []
     if validation_data:
-        if learning_rate_decay:
-            decay = K.callbacks.LearningRateScheduler(lambda epoch: alpha/ (1 + decay_rate * epoch))
-            callbacks.append(decay)
-        if early_stopping:
-            early_stopping = K.callbacks.EarlyStopping(
-            patience=patience
-        )
-        callbacks.append(early_stopping)
+        callback.append(K.callbacks.LearningRateScheduler
+                        (alpha / (1 + decay_rate * epochs)))
+    if early_stopping and validation_data:
+        callback.append(K.callbacks.EarlyStopping
+                        (patience=patience))
     return network.fit(x=data,
                        y=labels,
                        batch_size=batch_size,
@@ -29,4 +26,4 @@ def train_model(network, data, labels, batch_size, epochs,
                        verbose=verbose,
                        shuffle=shuffle,
                        validation_data=validation_data,
-                       callbacks=callbacks)
+                       callbacks=callback)
