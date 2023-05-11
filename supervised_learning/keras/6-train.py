668225788b38db415850948c7a63386ae20f3eb3
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-11 16:34:00 +0200

    Train

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -5,8 +5,10 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None,
-                early_stopping=False, patience=0, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
     """update the previous function and
     to also analyze validaiton data"""
     callbacks = []
