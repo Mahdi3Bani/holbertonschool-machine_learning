c278e345699890d198c9fcb60859544d326e7534
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-11 15:09:14 +0200

    Train

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""update the previous function and to also analyze validaiton data"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, validation_data=None,
+                early_stopping=False, patience=0, verbose=True, shuffle=False):
+    """update the previous function and
+    to also analyze validaiton data"""
+    return network.fit(data, labels, batch_size=batch_size,
+                       epochs=epochs, validation_data=validation_data,
+                       early_stopping=early_stopping,patience=patience
+                       verbose=verbose, shuffle=shuffle)
