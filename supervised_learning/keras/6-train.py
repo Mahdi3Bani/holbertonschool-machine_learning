224a2135b40959c098537ceba253d9d896f681c0
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-11 16:21:59 +0200

    Train

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -9,13 +9,14 @@ def train_model(network, data, labels, batch_size, epochs, validation_data=None,
                 early_stopping=False, patience=0, verbose=True, shuffle=False):
     """update the previous function and
     to also analyze validaiton data"""
-    callbacks = [] 
+    callbacks = []
     if early_stopping and validation_data:
         early_stopping = K.callbacks.EarlyStopping(
             monitor='val_loss',
             patience=patience
         )
+        callbacks.append(early_stopping)
     return network.fit(data, labels, batch_size=batch_size,
                        epochs=epochs, validation_data=validation_data,
-                       early_stopping=early_stopping,patience=patience,
+                       early_stopping=early_stopping, patience=patience,
                        verbose=verbose, shuffle=shuffle, callbacks=callbacks)
