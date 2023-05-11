1ee23bfaf0e36205abce9538dbc66dcaed17de29
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-11 14:27:25 +0200

    input

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -13,18 +13,18 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     X = Y = K.Input(shape=(nx,))
 
-    regularization = K.regularizers.l2(lambtha)
+    
 
     for i in range(len(layers)):
         dense = (K.layers.
                  Dense(layers[i],
                        activation=activations[i],
-                       kernel_regularizer=regularization))
+                       kernel_regularizer=K.regularizers.l2(lambtha)))
         Y = dense(Y)
 
         if i < len(layers) - 1:
             dropout = K.layers.Dropout(1 - keep_prob)
 
-            y = dropout(Y)
+            Y = dropout(Y)
 
     return K.Model(inputs=X, outputs=Y)
