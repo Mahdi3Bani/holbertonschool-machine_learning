d926b8414457f5e394d9a171711621d2408d0df3
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-12 00:00:21 +0200

    save and load

---
+++ supervised_learning/keras/11-config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""save and load functions"""
+
+
+import tensorflow.keras as K
+
+
+def save_weights(network, filename):
+    """function to save the model"""
+    with open(filename, 'w') as f:
+        f.write(network.to_json())
+
+
+def load_config(filename):
+    '''function to loas a model from a filepath'''
+    with open(filename, 'w') as f:
+        return K.models.model_from_json(f.read())
