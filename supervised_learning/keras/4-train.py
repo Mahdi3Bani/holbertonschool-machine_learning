48f0530aee892a79ba5f9f2412715f3855e44fc1
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-11 14:58:28 +0200

    Train

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""converts a label vector into a one-hot matrix"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, verbose=True, shuffle=False):
+    """converts a label vector into a one-hot matrix"""
+    return network.fit(data, labels, batch_size=batch_size,
+                       epochs=epochs, verbose=verbose, shuffle=shuffle)
