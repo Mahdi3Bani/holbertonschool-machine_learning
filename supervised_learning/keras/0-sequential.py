560ce72aa3fedf67b73c6894281865e99371411c
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-11 14:14:25 +0200

    sequantial

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -17,7 +17,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         model.add(K.layers.Dropout(1 - keep_prob))
 
 
-    for i in range(1, len(layers)):
+    for i in range(1, len(layers) - 1):
         model.add(K.layers.
                   Dense(layers[i],
                         activation=activations[i],
