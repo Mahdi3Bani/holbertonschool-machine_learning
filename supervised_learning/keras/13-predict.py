ea81b932c858a820dc38cbbcc3b9de26f048b11f
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-12 00:28:19 +0200

    predicting

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""makes a prediction using a neural network"""
+
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """makes a prediction using a neural network"""
+
+    return network.predict(x=data, verbose=verbose)
