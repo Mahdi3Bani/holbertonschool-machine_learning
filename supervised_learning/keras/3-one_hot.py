6772a82f9a11393b4e2ff03f937ee647943753f2
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-11 14:51:22 +0200

    Adam

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""converts a label vector into a one-hot matrix"""
+
+
+import numpy as np
+
+
+def one_hot(labels, classes=None):
+    """converts a label vector into a one-hot matrix"""
+    if classes is None:
+        classes = np.max(labels) + 1
+
+    one_hot_matrix = np.zeros((len(labels), classes))
+    one_hot_matrix[np.arange(len(labels)), labels] = 1
+
+    return one_hot_matrix
