f85f20f3b2b3ec8cb324d1e197ac8ae76c32417d
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-11 14:11:27 +0200

    sequantial

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -13,6 +13,9 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     model.add(K.layers.Dense(layers[0],
                              activation=activations[0], input_shape=(nx,),
                              kernel_regularizer=K.regularizers.l2(lambtha)))
+    if len(layers) > 1:
+        model.add(K.layers.Dropout(1 - keep_prob))
+
 
     for i in range(1, len(layers)):
         model.add(K.layers.
