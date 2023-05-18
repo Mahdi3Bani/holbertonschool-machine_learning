aae6e36c8bb048b7f8666145b334b003acc5faca
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-18 02:49:53 +0200

    pool

---
+++ math/convolutions_and_pooling/6-pool.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""that performs a convolution on grayscale images"""
+
+
+import numpy as np
+
+
+def pool(images, kernel_shape, stride, mode='max'):
+    """that performs a convolution on grayscale images"""
+    # Get the shape of the input images and the kernel
+    m, h, w, c = images.shape
+    kh, kw = kernel_shape
+
+    sh, sw = stride
+
+    # Compute the output shape
+    oh = int((h - kh ) / sh + 1)
+    ow = int((w - kw ) / sw + 1)
+
+    # Initialize the output array
+    output = np.zeros(shape=(m, oh, ow, c))
+
+
+    for x in range(oh):
+        for y in range(ow):
+            if mode == "max":
+                output[:, x, y, :] = images\
+                [:, x *sh:w *sh + kh, y * sh:y * sh + kw, :].max(axis=(1, 2))
+            else:
+                output[:, x, y, :] = np.mean(
+                    images[:, x *sh:w *sh + kh, y * sh:y * sh + kw, :],
+                    axis=(1, 2)
+                )
+
+
+    return output
