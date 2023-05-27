3752899eaa1a010a70a54c4edd0699c0074b4b7a
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-28 00:31:27 +0200

    Inception Block

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -39,12 +39,14 @@ def inception_block(A_prev, filters):
     conv3x3_reduce = K.layers.Conv2D(filters=F3R, kernel_size=(1, 1),
                                      padding='same', activation='relu')(A_prev)
     conv3x3 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
-                              padding='same', activation='relu')(conv3x3_reduce)
+                              padding='same',
+                              activation='relu')(conv3x3_reduce)
 
     conv5x5_reduce = K.layers.Conv2D(filters=F5R, kernel_size=(1, 1),
                                      padding='same', activation='relu')(A_prev)
     conv5x5 = K.layers.Conv2D(filters=F5, kernel_size=(5, 5),
-                              padding='same', activation='relu')(conv5x5_reduce)
+                              padding='same',
+                              activation='relu')(conv5x5_reduce)
 
     pool = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(1, 1),
                                  padding='same')(A_prev)
