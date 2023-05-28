3338f75adb9b088f31244791b57cac69f30624f3
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-28 13:59:01 +0200

    Projection Block

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""Identity Block"""
+"""Projection Block
+"""
 
 
 import tensorflow.keras as K
+
 
 def projection_block(A_prev, filters, s=2):
     """
@@ -37,7 +39,8 @@ def projection_block(A_prev, filters, s=2):
 
     # Main path
     # First layer
-    conv1 = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), strides=(s, s),
+    conv1 = K.layers.Conv2D(filters=F11, kernel_size=(1, 1),
+                            strides=(s, s),
                             padding='same', kernel_initializer='he_normal')(A_prev)
     bn1 = K.layers.BatchNormalization(axis=3)(conv1)
     relu1 = K.layers.Activation('relu')(bn1)
@@ -54,7 +57,8 @@ def projection_block(A_prev, filters, s=2):
     bn3 = K.layers.BatchNormalization(axis=3)(conv3)
 
     # Shortcut path
-    shortcut = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(s, s),
+    shortcut = K.layers.Conv2D(filters=F12, kernel_size=(1, 1),
+                               strides=(s, s),
                                padding='same', kernel_initializer='he_normal')(shortcut)
     shortcut = K.layers.BatchNormalization(axis=3)(shortcut)
 
