48af875bc089e4d61734b13ad1d5123f21492d68
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-05-28 02:08:34 +0200

    readme ><

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -19,24 +19,24 @@ def inception_network():
 
     # Second conv layer
     conv2 = K.layers.Conv2D(filters=64,
-                             kernel_size=(1, 1),
-                             padding="same",
-                             activation="relu")(pool1)
+                            kernel_size=(1, 1),
+                            padding="same",
+                            activation="relu")(pool1)
     conv3 = K.layers.Conv2D(filters=192,
-                             kernel_size=(3, 3),
-                             padding="same",
-                             activation="relu")(conv2)
+                            kernel_size=(3, 3),
+                            padding="same",
+                            activation="relu")(conv2)
     pool2 = K.layers.MaxPooling2D(pool_size=(3, 3),
-                                       strides=(2, 2),
-                                       padding="same")(conv3)
+                                  strides=(2, 2),
+                                  padding="same")(conv3)
 
     # Inception blocks
     inception2a = inception_block(pool2, [64, 96, 128, 16, 32, 32])
     inception2b = inception_block(inception2a, [128, 128, 192, 32, 96, 64])
 
     pool3 = K.layers.MaxPooling2D(pool_size=(3, 3),
-                                       strides=(2, 2),
-                                       padding="same")(inception2b)
+                                  strides=(2, 2),
+                                  padding="same")(inception2b)
 
     inception4a = inception_block(pool3, [192, 96, 208, 16, 48, 64])
     inception4b = inception_block(inception4a, [160, 112, 224, 24, 64, 64])
@@ -45,14 +45,14 @@ def inception_network():
     inception4e = inception_block(inception4d, [256, 160, 320, 32, 128, 128])
 
     pool4 = K.layers.MaxPooling2D(pool_size=(3, 3),
-                                       strides=(2, 2),
-                                       padding="same")(inception4e)
+                                  strides=(2, 2),
+                                  padding="same")(inception4e)
 
     inception5a = inception_block(pool4, [256, 160, 320, 32, 128, 128])
     inception5b = inception_block(inception5a, [384, 192, 384, 48, 128, 128])
     # Average pooling layer
     avg_pool = K.layers.AvgPool2D(pool_size=(7, 7),
-                                     padding="same")(inception5b)
+                                  padding="same")(inception5b)
     dropout = K.layers.Dropout(rate=0.4)(avg_pool)
     # Fully connected layer
     fc = K.layers.Dense(units=1000, activation='softmax')(dropout)
