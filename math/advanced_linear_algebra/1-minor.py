20901f59d4881660ca2f749d79780d28953d3ee7
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-06-27 14:46:27 +0200

    minors

---
+++ math/advanced_linear_algebra/1-minor.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""minor matrix of a matrix"""
+
+
+def minor(matrix):
+    """calculates the minor matrix of a matrix:
+
+        matrix is a list of lists whose minor matrix
+        should be calculated
+
+        If matrix is not a list of lists, raise a
+        TypeError with the message matrix must be a
+        list of lists
+
+        If matrix is not square or is empty, raise a
+        ValueError with the message matrix must be a
+        non-empty square matrix
+
+        Returns: the minor matrix of matrix
+    """
+
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    for sub_list in matrix:
+        if not isinstance(sub_list, list):
+            raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) != len(matrix[0]) or len(matrix) == 0:
+        raise ValueError("matrix must be a non-empty square matrix")
+    for i in matrix:
+        if len(i) != len(matrix) or len(matrix) == 0:
+
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    if len(matrix) == 1:
+        return [[1]]
+
+    if len(matrix) == 2:
+        return [[matrix[1][1], matrix[1][0]], [matrix[0][1], matrix[0][0]]]
+
+    minors_mat = []
+    for i in range(len(matrix)):
+        minors_row = []
+        for j in range(len(matrix[i])):
+            sub_matrix = [row[:j] + row[j + 1:]
+                          for row in matrix[:i] + matrix[i + 1:]]
+            det = determinant(sub_matrix)
+            minors_row.append(det)
+        minors_mat.append(minors_row)
+
+    return minors_mat
+
+
+def determinant(matrix):
+    if len(matrix) == 1:
+        return matrix[0][0]
+    elif len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    det = 0
+    for j in range(len(matrix[0])):
+        sub_matrix = [row[1:] for row in matrix[1:]]
+        det += (-1) ** j * matrix[0][j] * determinant(sub_matrix)
+        return det
