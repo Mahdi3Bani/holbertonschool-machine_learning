f93f0c42bec9522ef3c8fe70176c32fa0ad96493
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-06-27 15:24:07 +0200

    cofactor

---
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""minor matrix of a matrix"""
+determinant = __import__('0-determinant').determinant
+
+
+def cofactor(matrix):
+    """calculates the minor matrix of a matrix:
+
+        matrix is a list of lists whose minor matrix
+        should be calculated
+
+        If matrix is not a list of lists, raise a
+        TypeError with the message matrix must be a
+        list of lists
+
+        If matrix is not square or is empty, raise a
+        ValueError with the message matrix must be a
+        non-empty square matrix
+
+        Returns: the minor matrix of matrix
+    """
+
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    for sub_list in matrix:
+        if not isinstance(sub_list, list):
+            raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) != len(matrix[0]) or len(matrix) == 0:
+        raise ValueError("matrix must be a non-empty square matrix")
+    for i in matrix:
+        if len(i) != len(matrix) or len(matrix) == 0:
+
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    if len(matrix) == 1:
+        return [[1]]
+
+    if len(matrix) == 2:
+        return [[matrix[1][1], -matrix[1][0]], [-matrix[0][1], matrix[0][0]]]
+
+    sign = 1
+    cofactor_mat = []
+    for i in range(len(matrix)):
+        cofactor_row = []
+        for j in range(len(matrix[i])):
+            sub_matrix = [row[:j] + row[j + 1:]
+                          for row in matrix[:i] + matrix[i + 1:]]
+            det = determinant(sub_matrix) * sign
+            sign *= -1
+            cofactor_row.append(det)
+        cofactor_mat.append(cofactor_row)
+
+    return cofactor_mat
