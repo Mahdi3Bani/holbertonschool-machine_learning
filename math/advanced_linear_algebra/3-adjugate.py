bc30fb2df5f0a873054496aee7734c1c1806fd6c
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-06-29 11:57:25 +0200

    adjugate

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -37,9 +37,6 @@ def adjugate(matrix):
     if len(matrix) == 1:
         return [[1]]
 
-    if len(matrix) == 2:
-        return [[matrix[1][1], -matrix[1][0]], [-matrix[0][1], matrix[0][0]]]
-
     cofactor_matrix = cofactor(matrix)
     adjugate_matrix = []
     for i in range(len(matrix)):
