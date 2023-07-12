bc31e98938366d654a2ce1a1e29732a6e2f7ade4
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-07-12 17:38:33 +0200

    initialize

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -6,15 +6,16 @@ import numpy as np
 
 def initialize(X, k):
     '''comment'''
-    if k <= 0:
+    if not isinstance(k, int) or k <= 0:
         return None
-    
+    if not isinstance(X, np.ndarray):
+        return None
     n, d = X.shape
     if n < k:
         return None
-    
+
     min_values = np.min(X, axis=0)
     max_values = np.max(X, axis=0)
-    
+
     centroids = np.random.uniform(low=min_values, high=max_values, size=(k, d))
     return centroids
