863d1466e2fd6a8b49c93c71c7439968bb2e718e
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-07-17 00:25:18 +0200

    k means

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -2,7 +2,23 @@
 """comment"""
 
 import numpy as np
-initialize = __import__('0-initialize').initialize
+
+
+def initialize(X, k):
+    '''comment'''
+    if not isinstance(k, int) or k <= 0:
+        return None
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None
+    n, d = X.shape
+    if n < k:
+        return None
+
+    min_values = np.min(X, axis=0)
+    max_values = np.max(X, axis=0)
+
+    centroids = np.random.uniform(low=min_values, high=max_values, size=(k, d))
+    return centroids
 
 
 def kmeans(X, k, iterations=1000):
