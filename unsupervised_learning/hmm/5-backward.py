481c8e5fb124d8d4394dcf7b6d3fb52e2f27880f
Mahdi3Bani <bani.mahdi666@gmail.com>
2023-07-23 23:27:52 +0200

    backward

---
+++ unsupervised_learning/hmm/5-backward.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''comment'''
+
+
+import numpy as np
+
+
+def backward(Observation, Emission, Transition, Initial):
+    T = Observation.shape[0]
+    N, M = Emission.shape
+
+    if T == 0 or N == 0 or M == 0:
+        return None, None
+
+    backward_matrix = np.zeros((N, T))
+
+    backward_matrix[:, -1] = 1
+
+    for t in range(T - 2, -1, -1):
+        for i in range(N):
+            probabilities = Transition[i, :] * Emission[:, Observation[t + 1]] * backward_matrix[:, t + 1]
+            backward_matrix[i, t] = np.sum(probabilities)
+
+    P = np.sum(Initial.reshape(-1) * Emission[:, Observation[0]] * backward_matrix[:, 0])
+
+    return P, backward_matrix
