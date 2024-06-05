905b4839034840b469c764a565d957ef2020241b
Mahdi3Bani <bani.mahdi666@gmail.com>
2024-06-05 21:54:33 +0100

    embeddings

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -12,6 +12,7 @@ def preprocess_sentence(sentence):
     words = re.findall(r'\w+', processed_sentence)
     return words
 
+
 def build_vocabulary(sentences):
     """
     Build vocabulary from preprocessed sentences.
@@ -22,6 +23,7 @@ def build_vocabulary(sentences):
             all_words.add(word)
     return sorted(all_words)
 
+
 def create_word_index(vocab):
     """
     Create a dictionary mapping words to their indices in the vocabulary
@@ -31,6 +33,7 @@ def create_word_index(vocab):
         word_to_index[word] = idx
     return word_to_index
 
+
 def count_words(sentences, word_to_index):
     """
     Count the occurrences of each word in each sentence
@@ -38,13 +41,14 @@ def count_words(sentences, word_to_index):
     num_sentences = len(sentences)
     num_features = len(word_to_index)
     embeddings = np.zeros((num_sentences, num_features), dtype=int)
-    
+
     for i, sentence in enumerate(sentences):
         for word in sentence:
             if word in word_to_index:
                 embeddings[i, word_to_index[word]] += 1
-                
+
     return embeddings
+
 
 def bag_of_words(sentences, vocab=None):
     """
@@ -53,12 +57,12 @@ def bag_of_words(sentences, vocab=None):
     preprocessed_sentences = []
     for sentence in sentences:
         preprocessed_sentences.append(preprocess_sentence(sentence))
-    
+
     if vocab is None:
         vocab = build_vocabulary(preprocessed_sentences)
-    
+
     word_to_index = create_word_index(vocab)
-    
+
     embeddings = count_words(preprocessed_sentences, word_to_index)
-    
+
     return embeddings, vocab
