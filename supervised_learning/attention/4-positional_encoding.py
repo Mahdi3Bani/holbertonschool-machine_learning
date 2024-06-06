3ae73116f929e0a86952517389532f6d65b48ffd
Mahdi3Bani <bani.mahdi666@gmail.com>
2024-06-06 14:15:23 +0200

    fixing the loop

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -12,6 +12,6 @@ def positional_encoding(max_seq_len, dm):
     for pos in range(max_seq_len):
         for i in range(dm//2):
             positional_encoding_matrix[pos, 2*i] = np.sin(pos / (10000 ** ((2 * i) / dm)))
-            positional_encoding_matrix[pos, 2*i + 1] = np.cos(pos / (10000 ** ((2 * (i + 1)) / dm)))
+            positional_encoding_matrix[pos, 2*i + 1] = np.cos(pos / (10000 ** ((2 * i) / dm)))
 
     return positional_encoding_matrix
