43da7c67cd6a1ea6a7a6df092f1accfac7c2b041
Mahdi3Bani <bani.mahdi666@gmail.com>
2024-06-06 14:17:22 +0200

    fixing pycode

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-import numpy as np
-"""
+"""positioanl encoding function"""
 
-"""
+
+import numpy as np
+
 
 
 def positional_encoding(max_seq_len, dm):
@@ -11,7 +12,9 @@ def positional_encoding(max_seq_len, dm):
 
     for pos in range(max_seq_len):
         for i in range(dm//2):
-            positional_encoding_matrix[pos, 2*i] = np.sin(pos / (10000 ** ((2 * i) / dm)))
-            positional_encoding_matrix[pos, 2*i + 1] = np.cos(pos / (10000 ** ((2 * i) / dm)))
+            positional_encoding_matrix[pos, 2*i] = np.sin(
+                pos / (10000 ** ((2 * i) / dm)))
+            positional_encoding_matrix[pos, 2*i + 1] = np.cos(
+                pos / (10000 ** ((2 * i) / dm)))
 
     return positional_encoding_matrix
