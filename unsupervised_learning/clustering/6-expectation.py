c79e384f167b6f39e03a8d1815065b4a399231f8
Mahdi3Bani <bani.mahdi666@gmail.com>
2024-06-08 17:35:03 +0200

    fixed the tasks

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -8,19 +8,26 @@ def expectation(X, pi, m, S):
     Calculate the expectation step in the EM algorithm for a GMM.
     """
 
-    n, d = X.shape
-    k = m.shape[0]
-
-    # Initialize the posterior probability matrix
-    g = np.zeros((k, n))
-
-    for i in range(k):
-
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+        return None, None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+        return None, None
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None, None
+    if m.shape[1] != X.shape[1]:
+        return None, None
+    if S.shape[1] != X.shape[1] or X.shape[1] != S.shape[2]:
+        return None, None
+    if pi.shape[0] != m.shape[0] or pi.shape[0] != S.shape[0]:
+        return None, None
+    if not np.isclose([np.sum(pi)], [1])[0]:
+        return None, None
+    g = np.zeros((m.shape[0], X.shape[0]))
+    for i in range(m.shape[0]):
         g[i] = pdf(X, m[i], S[i]) * pi[i]
-
     g_sum = np.sum(g, axis=0)
     g /= g_sum
-
-    l = np.sum(np.log(g_sum))
-
-    return g, l
+    loo = np.sum(np.log(g_sum))
+    return g, loo
--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -10,17 +10,24 @@ def maximization(X, g):
     """
     Calculates the maximization step in the EM algorithm for a GMM
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if not isinstance(g, np.ndarray) or len(g.shape) != 2:
+        return None, None, None
+    if g.shape[1] != X.shape[0]:
+        return None, None, None
+    cluster = np.sum(g, axis=0)
+    cluster = np.sum(cluster)
+    if int(cluster) != X.shape[0]:
+        return None, None, None
     n, d = X.shape
     k = g.shape[0]
-
-    pi = np.sum(g, axis=1) / n
-
-    m = np.dot(g, X) / np.sum(g, axis=1)[:, np.newaxis]
-
-    cov_mat = np.zeros((k, d, d))
-
-    for j in range(k):
-        diff = X - m[j]
-        cov_mat[j] = np.dot(g[j] * diff.T, diff) / np.sum(g[j])
-
+    pi = np.zeros((k,))
+    m = np.zeros((k, d))
+    S = np.zeros((k, d, d))
+    for i in range(k):
+        pi[i] = np.sum(g[i])/n
+        m[i] = np.matmul(g[i], X)/np.sum(g[i])
+        distance = X - m[i]
+        S[i] = np.matmul(g[i] * distance.T, distance)/np.sum(g[i])
     return pi, m, S
