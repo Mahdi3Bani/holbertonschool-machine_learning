6c86227dae7b63f594bc474fe0c42d86c12a30cd
Mahdi3Bani <bani.mahdi666@gmail.com>
2024-06-12 21:25:31 +0200

    finxing the import problem

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -2,39 +2,39 @@
 '''creates an autoencoder'''
 
 
-import tensorflow as tf
-from tensorflow.keras import layers, models, optimizers, losses
+import tensorflow.keras as keras
+
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """
     Builds an autoencoder model
     """
 
-    encoder_input = layers.Input(shape=(input_dims,))
+    encoder_input = keras.layers.Input(shape=(input_dims,))
 
     x = encoder_input
     for units in hidden_layers:
-        x = layers.Dense(units, activation='relu')(x)
+        x = keras.layers.Dense(units, activation='relu')(x)
 
-    encoder_output = layers.Dense(latent_dims, activation='relu')(x)
+    encoder_output = keras.layers.Dense(latent_dims, activation='relu')(x)
 
-    encoder = models.Model(encoder_input, encoder_output, name='encoder')
+    encoder = keras.models.Model(encoder_input, encoder_output, name='encoder')
 
-    decoder_input = layers.Input(shape=(latent_dims,))
+    decoder_input = keras.layers.Input(shape=(latent_dims,))
 
     x = decoder_input
     for units in reversed(hidden_layers):
-        x = layers.Dense(units, activation='relu')(x)
+        x = keras.layers.Dense(units, activation='relu')(x)
 
-    decoder_output = layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
 
-    decoder = models.Model(decoder_input, decoder_output, name='decoder')
+    decoder = keras.models.Model(decoder_input, decoder_output, name='decoder')
 
     autoencoder_input = encoder_input
     encoded = encoder(autoencoder_input)
     decoded = decoder(encoded)
-    auto = models.Model(autoencoder_input, decoded, name='autoencoder')
+    auto = keras.models.Model(autoencoder_input, decoded, name='autoencoder')
 
-    auto.compile(optimizer=optimizers.Adam(), loss=losses.BinaryCrossentropy())
+    auto.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.BinaryCrossentropy())
 
     return encoder, decoder, auto
