e4a931f2a7a95008c63bc30d323f173c70651904
Mahdi3Bani <bani.mahdi666@gmail.com>
2024-06-13 00:18:17 +0200

    dixing doc problem

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -2,23 +2,24 @@
 """api module"""
 import requests
 
-
-if __name__ == '__main__':
-    launches = requests.get(
-        'https://api.spacexdata.com/v4/launches/upcoming').json()
+def first_launch():
+    """Get details of the upcoming SpaceX launch."""
+    launches = requests.get('https://api.spacexdata.com/v4/launches/upcoming').json()
     unix_dates = [launch['date_unix'] for launch in launches]
     min_idx = unix_dates.index(min(unix_dates))
     upcoming_launch = launches[min_idx]
 
-    rocket = requests.get(
-        'https://api.spacexdata.com/v4/rockets/{}'.format(
-            upcoming_launch['rocket'])).json()
-    launchpad = requests.get(
-        'https://api.spacexdata.com/v4/launchpads/{}'.format(
-            upcoming_launch['launchpad'])).json()
+    rocket = requests.get(f"https://api.spacexdata.com/v4/rockets/{upcoming_launch['rocket']}").json()
+    launchpad = requests.get(f"https://api.spacexdata.com/v4/launchpads/{upcoming_launch['launchpad']}").json()
 
-    print('{} ({}) {} - {} ({})'.format(upcoming_launch['name'],
-                                        upcoming_launch['date_local'],
-                                        rocket['name'],
-                                        launchpad['name'],
-                                        launchpad['locality']))
+    return {
+        'launch_name': upcoming_launch['name'],
+        'launch_date': upcoming_launch['date_local'],
+        'rocket_name': rocket['name'],
+        'launchpad_name': launchpad['name'],
+        'launchpad_locality': launchpad['locality']
+    }
+
+if __name__ == '__main__':
+    details = first_launch()
+    print(f"{details['launch_name']} ({details['launch_date']}) {details['rocket_name']} - {details['launchpad_name']} ({details['launchpad_locality']})")
--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -3,20 +3,21 @@
 import requests
 from collections import defaultdict
 
-if __name__ == '__main__':
+def rocket_frequency():
+    """Get the number of launches for each rocket."""
     rockets = defaultdict(int)
     launches = requests.get('https://api.spacexdata.com/v4/launches').json()
 
     for launch in launches:
         rocket_id = launch.get('rocket')
-        rocket = requests.\
-            get('https://api.spacexdata.com/v4/rockets/{}'
-                .format(rocket_id)).json()
+        rocket = requests.get(f'https://api.spacexdata.com/v4/rockets/{rocket_id}').json()
         rocket_name = rocket.get('name')
         rockets[rocket_name] += 1
 
-    sorted_rockets = sorted(
-        rockets.items(), key=lambda kv: kv[1], reverse=True)
+    sorted_rockets = sorted(rockets.items(), key=lambda kv: kv[1], reverse=True)
+    return sorted_rockets
 
-    for rocket_name, count in sorted_rockets:
-        print('{}: {}'.format(rocket_name, count))
+if __name__ == '__main__':
+    rocket_launch_counts = rocket_frequency()
+    for rocket_name, count in rocket_launch_counts:
+        print(f'{rocket_name}: {count}')
