f828b3644f4f5f4cf1b79c933af6716a64700b41
Mahdi3Bani <bani.mahdi666@gmail.com>
2024-06-13 00:17:22 +0200

    dixing doc problem

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -3,21 +3,20 @@
 import requests
 from collections import defaultdict
 
-def rocket_frequency():
-    """Get the number of launches for each rocket."""
+if __name__ == '__main__':
     rockets = defaultdict(int)
     launches = requests.get('https://api.spacexdata.com/v4/launches').json()
 
     for launch in launches:
         rocket_id = launch.get('rocket')
-        rocket = requests.get(f'https://api.spacexdata.com/v4/rockets/{rocket_id}').json()
+        rocket = requests.\
+            get('https://api.spacexdata.com/v4/rockets/{}'
+                .format(rocket_id)).json()
         rocket_name = rocket.get('name')
         rockets[rocket_name] += 1
 
-    sorted_rockets = sorted(rockets.items(), key=lambda kv: kv[1], reverse=True)
-    return sorted_rockets
+    sorted_rockets = sorted(
+        rockets.items(), key=lambda kv: kv[1], reverse=True)
 
-if __name__ == '__main__':
-    rocket_launch_counts = rocket_frequency()
-    for rocket_name, count in rocket_launch_counts:
-        print(f'{rocket_name}: {count}')
+    for rocket_name, count in sorted_rockets:
+        print('{}: {}'.format(rocket_name, count))
