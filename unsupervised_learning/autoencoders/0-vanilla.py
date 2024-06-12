af8e393b6ffa1c4cde828bd0a48f27646e19e61f
Mahdi3Bani <bani.mahdi666@gmail.com>
2024-06-12 21:33:48 +0200

    finxing pycode

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env python3
 """autoencoder"""
-import tensorflow.keras as K
+import tensorflow.keras as keras
+
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """
     Creates a vanilla autoencoder model.
     """
-    
     # Encoder
-    encoder_input = K.Input(shape=(input_dims,))
+    encoder_input = keras.Input(shape=(input_dims,))
     x = encoder_input
     for units in hidden_layers:
-        x = K.layers.Dense(units, activation='relu')(x)
-    latent_output = K.layers.Dense(latent_dims, activation='relu')(x)
-    encoder = K.Model(encoder_input, latent_output, name='encoder')
-    
+        x = keras.layers.Dense(units, activation='relu')(x)
+    latent_output = keras.layers.Dense(latent_dims, activation='relu')(x)
+    encoder = keras.Model(encoder_input, latent_output, name='encoder')
     # Decoder
-    decoder_input = K.Input(shape=(latent_dims,))
+    decoder_input = keras.Input(shape=(latent_dims,))
     x = decoder_input
     for units in reversed(hidden_layers):
-        x = K.layers.Dense(units, activation='relu')(x)
-    decoder_output = K.layers.Dense(input_dims, activation='sigmoid')(x)
-    decoder = K.Model(decoder_input, decoder_output, name='decoder')
-    
+        x = keras.layers.Dense(units, activation='relu')(x)
+    decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder = keras.Model(decoder_input, decoder_output, name='decoder')
     # Autoencoder
     autoencoder_input = encoder_input
     encoded = encoder(autoencoder_input)
     decoded = decoder(encoded)
-    autoencoder = K.Model(autoencoder_input, decoded, name='autoencoder')
-    
+    autoencoder = keras.Model(autoencoder_input, decoded, name='autoencoder')
     # Compile the autoencoder
     autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-    
     return encoder, decoder, autoencoder
